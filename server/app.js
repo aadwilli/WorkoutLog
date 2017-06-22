db169d836c61e6f4ef5cb6d5ed1264e1cd5d71ee
aadwilli <aadwilli15@gmail.com>
2017-06-22 14:24:29 -0400

    Server set up

---
+++ server/app.js
@@ -0,0 +1,8 @@
+//importing the express module
+var express = require('express');
+//calling the express function in the app variable
+var app = express();
+//adding the app.listen so the server will start up when it's run on Port 3000
+app.listen(3000, function(){
+	console.log("app is listening on 3000");
+});
