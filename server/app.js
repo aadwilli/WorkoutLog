feb48f75dea5e87e2472a519a510ee0bd7572f86
aadwilli <aadwilli15@gmail.com>
2017-06-23 14:06:33 -0400

    test api

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@
 var express = require('express');
 //calling the express function in the app variable
 var app = express();
+
+app.use('/api/test', function(req, res){
+	res.send("Hello World");
+});
 //adding the app.listen so the server will start up when it's run on Port 3000
 app.listen(3000, function(){
 	console.log("app is listening on 3000");
