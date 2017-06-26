a1615a4da5f43a79b273b6558efd1c5ec8705097
aadwilli <aadwilli15@gmail.com>
2017-06-26 13:45:40 -0400

    added security

--- server/app.js
+++ server/app.js
@@ -3,13 +3,15 @@ var app = express(); //calling the express function in the app variable
 var bodyParser = require('body-parser'); //This package will help the server parse out incoming requests that are easier to work with
 var sequelize = require('./db.js'); //importing the db.js module back in
 
-var User = sequelize.import(__dirname + '/models/user'); //imports sequelize while importing the user model
+var User = sequelize.import('./models/user'); //imports sequelize while importing the user model
 //creates a table in postgres and matches the model we defined
 User.sync() // sync({force:true}) WARNING: this will DROP the table each time the app starts!
 //telling the app to use bodyParser//parse data off incoming requests and turn it into JSON. //Takes the JSON and exposes it to be used for req.body
 app.use(bodyParser.json());
 app.use(require('./middleware/headers')); //importing the header file
 app.use('/api/user', require('./routes/user'));
+//login route
+app.use('/api/login', require('./routes/session'));
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
 });
