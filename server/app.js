0a810011e8a65130d29ccb736f1f3696f6c9c454
aadwilli <aadwilli15@gmail.com>
2017-06-26 10:42:48 -0400

    server refactoring

--- server/app.js
+++ server/app.js
@@ -1,11 +1,10 @@
-//importing the express module
-var express = require('express');
-//calling the express function in the app variable
-var app = express();
-//This package will help the server parse out incoming requests that are easier to work with
-var bodyParser = require('body-parser');
-//importing the header file
-app.use(require('./middleware/headers'));
+var express = require('express'); //importing the express module
+var app = express(); //calling the express function in the app variable
+var bodyParser = require('body-parser'); //This package will help the server parse out incoming requests that are easier to work with
+var sequelize = require('./db.js'); //importing the db.js module back in
+var User = sequelize.import('./models/user'); //imports sequelize while importing the user model
+
+app.use(require('./middleware/headers')); //importing the header file
 
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
@@ -15,34 +14,10 @@ app.listen(3000, function(){
 	console.log("app is listening on 3000");
 });
 
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('workoutlog', 'postgres', 'Letmein1234!', {
-	host: 'localhost',
-	dialect: 'postgres'
-});
-
-sequelize.authenticate().then(
-	function(){
-		console.log('connected to workoutlog postgres db');
-	},
-	function(err){
-		console.log(err);
-	}
-);
-
-//build a user model in sqllize
-	var User = sequelize.define('user', {
-		username: Sequelize.STRING,
-		passwordhash: Sequelize.STRING,
-	});
 //creates a table in postgres and matches the model we defined
-	User.sync()
-//DANGER!!!!This drops the table if need to do so. Leave commented for now
-// User.sync({force:true});
+User.sync() // sync({force:true}) WARNING: this will DROP the table each time the app starts!
 
-//telling the app to use bodyParser
-//parse data off incoming requests and turn it into JSON. 
-//Takes the JSON and exposes it to be used for req.body
+//telling the app to use bodyParser//parse data off incoming requests and turn it into JSON. //Takes the JSON and exposes it to be used for req.body
 app.use(bodyParser.json());
 //API endpoint handling incoming POST request
 app.post('/api/user', function(req, res){
